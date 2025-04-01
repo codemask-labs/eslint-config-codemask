0612908db4742d1db45babaa454a0a871ad110d9
ada-krupa <119587056+ada-krupa@users.noreply.github.com>
2025-04-01 15:40:13 +0200

    fix: remove unused package and disable no floating promises rule (#20)
    * fix: remove unused package
    
    * docs: update README.md
    
    * fix: disable no floating promises

--- eslint.config.js
+++ eslint.config.js
@@ -257,6 +257,7 @@ export default [
             ],
             '@typescript-eslint/restrict-template-expressions': 'error',
             '@typescript-eslint/no-unnecessary-condition': 'error',
+            '@typescript-eslint/no-floating-promises': 'off',
         },
     },
     {
